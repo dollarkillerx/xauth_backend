63b53133376b1df5ad07390cf9b40bd9d809a47c
dollarkillerx <adapawang@gmail.com>
2025-05-01 12:44:56 +0900

    add auth

--- internal/user/service.go
+++ internal/user/service.go
@@ -2,6 +2,9 @@ package user
 
 import (
 	"context"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+
 	"github.com/dollarkillerx/xauth_backend/api/user"
 	"github.com/dollarkillerx/xauth_backend/internal/conf"
 	"github.com/dollarkillerx/xauth_backend/internal/storage"
@@ -14,7 +17,50 @@ type UserService struct {
 	user.UnimplementedUserServiceServer
 }
 
+func (u *UserService) RegisterStudent(ctx context.Context, request *user.RegisterStudentRequest) (*user.RegisterStudentResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (u *UserService) RegisterTeacher(ctx context.Context, request *user.RegisterTeacherRequest) (*user.RegisterTeacherResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
 func (u *UserService) Login(ctx context.Context, request *user.LoginRequest) (*user.LoginResponse, error) {
+	jwt, err := generateJWT(request.Email, u.Conf.JWTSecretKey, time.Hour*24)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user.LoginResponse{
+		Jwt: jwt,
+	}, nil
+}
+
+func generateJWT(userID string, secretKey string, duration time.Duration) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(duration).Unix(), // 过期时间
+		"iat":     time.Now().Unix(),               // 签发时间
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(secretKey))
+}
+
+func (u *UserService) UserInfo(ctx context.Context, request *user.UserInfoRequest) (*user.UserInfoResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (u *UserService) UpdateUserAvatar(ctx context.Context, request *user.UpdateUserAvatarRequest) (*user.UpdateUserAvatarResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (u *UserService) mustEmbedUnimplementedUserServiceServer() {
 	//TODO implement me
 	panic("implement me")
 }
